156f4f043cce76420c9229ddacf5e9144ee0964a
Francois Villalba <villalbajunior@msn.com>
2023-11-28 12:11:07 +0100

    changement en cours

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -29,4 +29,42 @@ class DepositsController < ApplicationController
   def product_params
     params.require(:product).permit(:title, :category, :detail, :condition, :price)
   end
+
+  def price
+    
+    if product.condition == "Très Bon"
+      if product.category == "Divertissement"
+        puts "7"
+      elsif product.category == "Electrique" || "Sport"
+        puts "30"
+      else product.category == "Mobilier" || "Jardinage"
+        puts "20"
+      end
+    elsif product.condition == "Bon"
+      if product.category == "Divertissement"
+        puts "4"
+      elsif product.category == "Electrique" || "Sport"
+        puts "20"
+      else product.category == "Mobilier" || "Jardinage"
+        puts "15"
+      end
+    elsif product.condition == "Moyen"
+      if product.category == "Divertissement"
+        puts "2"
+      elsif product.category == "Electrique" || "Sport"
+        puts "15"
+      else product.category == "Mobilier" || "Jardinage"
+        puts "10"
+      end
+    else product.condition == "Usagé"
+      if product.category == "Divertissement"
+        puts "1"
+      elsif product.category == "Electrique" || "Sport"
+        puts "10"
+      else product.category == "Mobilier" || "Jardinage"
+        puts "5"
+      end
+    end
+  end
+
 end
