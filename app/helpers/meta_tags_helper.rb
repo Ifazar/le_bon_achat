13ea79db291f0a3593da51c718ed9899e8ecde37
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-12-07 12:16:08 +0100

    modif image

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -19,7 +19,7 @@ module MetaTagsHelper
 
     meta_image = (content_for?(:meta_image) ? content_for(:meta_image) :
     DEFAULT_META[“meta_image”])
-    
+
     # little twist to make it work equally with an asset or a url
 
     meta_image.starts_with?(“http”) ? meta_image : image_url(meta_image)
